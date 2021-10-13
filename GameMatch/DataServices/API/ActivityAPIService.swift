2ef170707fbd1df6fec3f464f51a73e6e9b5f4f3
Luke <lukeshi808@gmail.com>
2021-10-12 20:28:54 -0700

    update

--- GameMatch/DataServices/API/ActivityAPIService.swift
+++ GameMatch/DataServices/API/ActivityAPIService.swift
@@ -14,16 +14,16 @@ class ActivityAPIService: BaseAPIService
                     longitude: CLLocationDegrees,
                     completion: @escaping (Result<[Activity], Error>) -> Void)
     {
-        get(request: APIRequests.activities,
-            parameters: ["latitude": String(latitude),
-                         "longitude": String(longitude)],
-            completion: { (result: Result<Activities, Error>) in
+        post(request: APIRequests.activities,
+             parameters: ["latitude": latitude,
+                          "longitude": longitude],
+             completion: { (result: Result<Activities, Error>) in
                             switch result {
                             case .success(let activities):
                                 completion(.success(activities.activities))
                             case .failure(let error):
                                 completion(.failure(error))
                             }
-                        })
+                         })
     }
 }
