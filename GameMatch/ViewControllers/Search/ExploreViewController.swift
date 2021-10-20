b3eb70055568c94f915ca731307a15c5cd5aedc2
Luke <lukeshi808@gmail.com>
2021-10-19 20:53:35 -0700

    Clean up

--- GameMatch/ViewControllers/Search/ExploreViewController.swift
+++ GameMatch/ViewControllers/Search/ExploreViewController.swift
@@ -22,7 +22,6 @@ class ExploreViewController: BaseViewController
     
     private let exploreVM = ExploreViewModel()
     
-//    private var locationManager: CLLocationManager?
     private var searchCompleter = MKLocalSearchCompleter()
     private var locationSearchResults = [MKLocalSearchCompletion]()
     
@@ -90,11 +89,7 @@ class ExploreViewController: BaseViewController
     private func setupLocationService()
     {
         LocationService.shared.setupLocationManager()
-        
-//        locationManager = CLLocationManager()
-//        locationManager?.delegate = self
-//        locationManager?.requestWhenInUseAuthorization()
-        
+                
         searchCompleter.delegate = self
     }
     
@@ -390,47 +385,3 @@ extension ExploreViewController: MKMapViewDelegate
         }
     }
 }
-
-//extension ExploreViewController: CLLocationManagerDelegate
-//{
-//    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus)
-//    {
-//        switch status {
-//        case .denied: // Setting option: Never
-//            print("LocationManager didChangeAuthorization denied")
-//        case .notDetermined: // Setting option: Ask Next Time
-//            print("LocationManager didChangeAuthorization notDetermined")
-//        case .authorizedWhenInUse: // Setting option: While Using the App
-//            print("LocationManager didChangeAuthorization authorizedWhenInUse")
-//            locationManager?.requestLocation()
-//        case .authorizedAlways: // Setting option: Always
-//            print("LocationManager didChangeAuthorization authorizedAlways")
-//            locationManager?.requestLocation()
-//        case .restricted: // Restricted by parental control
-//            print("LocationManager didChangeAuthorization restricted")
-//        default:
-//            print("LocationManager didChangeAuthorization")
-//        }
-//    }
-//
-//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
-//    {
-//        print("LocationManager didUpdateLocations: numberOfLocation: \(locations.count)")
-//        let dateFormatter = DateFormatter()
-//        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-//
-//        locations.forEach { (location) in
-//            print("LocationManager didUpdateLocations: \(dateFormatter.string(from: location.timestamp)); \(location.coordinate.latitude), \(location.coordinate.longitude)")
-//        }
-//    }
-//
-//    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error)
-//    {
-//        print("LocationManager didFailWithError \(error.localizedDescription)")
-//        if let error = error as? CLError, error.code == .denied {
-//            // Location updates are not authorized.
-//            // To prevent forever looping of `didFailWithError` callback
-//            locationManager?.stopMonitoringSignificantLocationChanges()
-//        }
-//    }
-//}
