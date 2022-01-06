48b2f192dbfb7275f293c5a7324195afea3edd26
Luke <lukeshi808@gmail.com>
2022-01-06 01:21:14 -0800

    Profile screen

--- GameMatch/ViewControllers/BaseViewController.swift
+++ GameMatch/ViewControllers/BaseViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 import NVActivityIndicatorView
 import Combine
 
+typealias MenuAction = (title: String, actionStyle: UIAlertAction.Style, action: (() -> Void)?)
+
 class BaseViewController: UIViewController
 {
     var viewModel: BaseViewModel?
@@ -100,5 +102,47 @@ class BaseViewController: UIViewController
             nextScreen.modalPresentationStyle = .fullScreen
             present(nextScreen, animated: true, completion: nil)
         }
+    }
+
+    @discardableResult
+    func showAlertController(title: String? = nil,
+                             with selections: [MenuAction],
+                             enableCancel: Bool = true) -> UIAlertController
+    {
+        let menu = createMenu(title: title, selections: selections, withCancel: enableCancel)
+
+        // Below 2 lines will work only for iPad as popoverPresentationController only exists in iPad
+        menu.popoverPresentationController?.sourceView = self.view
+        menu.popoverPresentationController?.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.midY, width: 0, height: 0)
+
+        // Hide the directional arrow
+        menu.popoverPresentationController?.permittedArrowDirections = UIPopoverArrowDirection()
+
+        present(menu, animated: true, completion: nil)
+
+        return menu
+    }
+
+    private func createMenu(title: String?,
+                            selections: [MenuAction],
+                            withCancel: Bool) -> UIAlertController
+    {
+        let menu = UIAlertController(title: title, message: nil, preferredStyle: .actionSheet)
+
+        for selection in selections
+        {
+            let menuAction = UIAlertAction(title: selection.title, style: selection.actionStyle, handler: { _ in
+                selection.action?()
+            })
+            menu.addAction(menuAction)
+        }
+
+        if withCancel
+        {
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            menu.addAction(cancelAction)
+        }
+
+        return menu
     }
 }
--- GameMatch/ViewControllers/ProfileViewController.swift
+++ GameMatch/ViewControllers/ProfileViewController.swift
@@ -6,9 +6,15 @@
 //
 
 import UIKit
+import MobileCoreServices
+import AVFoundation
 
-class ProfileViewController: UIViewController
+class ProfileViewController: BaseViewController
 {
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var avatarView: UIImageView!
+    @IBOutlet weak var contactField: UITextField!
+
     private let profileVM = ProfileViewModel()
     
     override func viewDidLoad()
@@ -16,6 +22,19 @@ class ProfileViewController: UIViewController
         super.viewDidLoad()
 
         navigationItem.title = "Profile"
+
+        let avatarTapGesture = UITapGestureRecognizer(target: self, action: #selector(changePhoto))
+        avatarView.addGestureRecognizer(avatarTapGesture)
+    }
+
+    @objc private func changePhoto()
+    {
+        var selections: [MenuAction] = []
+        selections.append((title: "Take Picture", actionStyle: .default, action: self.openCamera))
+        selections.append((title: "From Photo Album", actionStyle: .default, action: self.readPhotoAlbum))
+
+        showAlertController(title: "Upload Photo",
+                            with: selections)
     }
     
     @IBAction func closeAction(_ sender: Any)
@@ -30,5 +49,82 @@ class ProfileViewController: UIViewController
         profileVM.logout()
         
         closeAction(sender)
+    }
+
+    private func openCamera()
+    {
+        if AVCaptureDevice.authorizationStatus(for: AVMediaType.video) == .authorized
+        {
+            takePicture()
+        }
+        else
+        {
+            AVCaptureDevice.requestAccess(for: AVMediaType.video) { [weak self] permissionGranted in
+                DispatchQueue.main.async {
+                    if permissionGranted {
+                        self?.takePicture()
+                    } else {
+                        print("To take a photo, allow app to access your camera in Settings")
+                    }
+                }
+            }
+        }
+    }
+
+    private func readPhotoAlbum()
+    {
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
+        imagePicker.mediaTypes = [kUTTypeImage as String]
+        imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+
+    private func takePicture()
+    {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .camera
+        picker.cameraDevice = .front
+        picker.mediaTypes = [kUTTypeImage as String]
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+    }
+}
+
+extension ProfileViewController: UITextFieldDelegate
+{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+extension ProfileViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate
+{
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any])
+    {
+        // Local variable inserted by Swift 4.2 migrator.
+        let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
+
+        if let chosenImage = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.editedImage)] as? UIImage {
+            avatarView.image = chosenImage
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+
+    // Helper function inserted by Swift 4.2 migrator.
+    private func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any]
+    {
+        return Dictionary(uniqueKeysWithValues: input.map { key, value in (key.rawValue, value) })
+    }
+
+    // Helper function inserted by Swift 4.2 migrator.
+    private func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String
+    {
+        return input.rawValue
     }
 }
